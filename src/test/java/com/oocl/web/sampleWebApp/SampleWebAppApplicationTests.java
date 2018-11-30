c3bdd2dafe53f2e50eab40f1cc417d35bb92d51e
Eddie Fong <fonged@oocl.com>
2018-11-30 14:14:32 +0800

    set length and assertThrows

--- src/test/java/com/oocl/web/sampleWebApp/SampleWebAppApplicationTests.java
+++ src/test/java/com/oocl/web/sampleWebApp/SampleWebAppApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
+
 import static com.oocl.web.sampleWebApp.AssertHelper.assertThrows;
 import static org.junit.Assert.assertEquals;
 
@@ -14,15 +16,18 @@ import static org.junit.Assert.assertEquals;
 public class SampleWebAppApplicationTests {
     @Autowired
     private SingleEntityRepository singleEntityReposity;
-
+    @Autowired
+    private EntityManager entityManager;
     @Test
     public void should_fetch_entity(){
         final SingleEntity singleEntity = new SingleEntity();
         singleEntity.id = 1L;
         singleEntity.name = "Name1";
         singleEntityReposity.save(singleEntity);
+        entityManager.flush();
 
         final SingleEntity fetched = singleEntityReposity.getOne(1L);
+        entityManager.clear();
 
         assertEquals("Name1",fetched.name);
     }
